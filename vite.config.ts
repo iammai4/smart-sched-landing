1f55ea8ea0c740396aee88b60accc5395d724c06
iammai4 <bunnolmai41@gmail.com>
2025-12-11 14:23:17 -0700

    test

--- vite.config.ts
+++ vite.config.ts
@@ -27,6 +27,7 @@ export default defineConfig({
     },
   },
   root: path.resolve(import.meta.dirname, "client"),
+  publicDir: "docs",
   build: {
     outDir: path.resolve(import.meta.dirname, "docs"),
     emptyOutDir: true,
