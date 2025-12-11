98905a4a6724fa3bbd0f4919918c3a35065f33d5
iammai4 <bunnolmai41@gmail.com>
2025-12-11 12:23:02 -0700

    try HashRouter for 404 error

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,10 @@
+import { HashRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+    <HashRouter>
+        <App />
+    </HashRouter>
+);
