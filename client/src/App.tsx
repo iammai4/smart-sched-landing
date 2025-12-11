a094599798be855608067de6b42372b3cc1ad832
iammai4 <bunnolmai41@gmail.com>
2025-12-11 12:33:01 -0700

    use react-router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,23 @@
-import { Switch, Route } from "wouter";
+// import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/not-found";
 import LandingPage from "@/pages/LandingPage";
+import { Routes, Route } from "react-router-dom";
+
 
 function Router() {
   return (
-    <Switch>
-      <Route path="/smart-sched-landing" component={LandingPage} />
-      <Route component={NotFound} />
-    </Switch>
+    <Routes>
+        {/* <Route index element={<StepOne />} />
+        <Route path="step-2" element={<StepTwo />} />
+        <Route path="step-3" element={<StepThree />} /> */}
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/smart-sched-landing" element={<LandingPage />} />
+        <Route path="*" element={<NotFound />} />
+    </Routes>
   );
 }
 
